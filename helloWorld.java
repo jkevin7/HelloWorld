2db4879a667f691521cc31aa13b3f94f0906f704
John Kevin Abonita <johnkevinabonita@gmail.com>
2018-04-16 11:32:45 +0800

    added Hello World Program

---
+++ helloWorld.java
@@ -0,0 +1,13 @@
+public class helloWorld {
+
+    public static void main(String[] args) {
+
+    	for (int i=1; i<=10; i++){
+
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello World!..."+i);
+
+	    }
+    }
+
+}
